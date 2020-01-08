b8d22d5728cb7a8f55b4faa17c5c643f3604afb3
cherryDevBomb <turcan.olguta@gmail.com>
2020-01-09 00:57:19 +0200

    created app main class and updated pom

---
+++ src/main/java/com/operacluj/registry/RegistryServicesApplication.java
@@ -0,0 +1,12 @@
+package com.operacluj.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryServicesApplication.class, args);
+    }
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,62 @@
+package model;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public class User {
+
+    private int id;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
